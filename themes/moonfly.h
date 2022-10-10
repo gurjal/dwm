aabf39dae61fc33f05ba9b16045f83dbd1718e80
gurjal <shaanraya135@gmail.com>
2022-10-10 12:24:20 -0700

    new nightfly theme added

--- themes/moonfly.h
+++ themes/moonfly.h
@@ -6,6 +6,6 @@
 static const char normal_fg[] = "#9e9e9e";
 static const char normal_bg[] = "#080808";
 static const char normal_bd[] = "#080808";
-static const char select_fg[] = "#eeeeee";
+static const char select_fg[] = "#e4e4e4";
 static const char select_bg[] = "#080808";
 static const char select_bd[] = "#b2ceee";
