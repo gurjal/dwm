f5c207328b2e87827de8605e84d16b5ce71b02fe
gurjal <shaanraya135@gmail.com>
2022-11-06 14:02:34 -0800

    add cfact support to column layout

--- dwm.c
+++ dwm.c
@@ -2331,9 +2331,16 @@ void
 column(Monitor *m)
 {
 	unsigned int i, n, h, w, x, y, mw;
+	float mfacts = 0, sfacts = 0;
 	Client *c;
 
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	// for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++) {
+		if (n < m->nmaster)
+			mfacts += c->cfact;
+		else
+			sfacts += c->cfact;
+	}
 	if (n == 0)
 		return;
 
@@ -2343,13 +2350,20 @@ column(Monitor *m)
 		mw = m->ww;
 	for (i = x = y = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
 		if (i < m->nmaster) {
-			w = (mw - x) / (MIN(n, m->nmaster) - i);
+			// h = (m->wh - my) * (c->cfact / mfacts);
+			// w = (mw - x) / (MIN(n, m->nmaster) - i);
+			w = (mw - x) * (c->cfact / mfacts);
 			resize(c, x + m->wx, m->wy, w - (2 * c->bw), m->wh - (2 * c->bw), 0);
-			x += WIDTH(c);
+			if (x + WIDTH(c) < m->ww)
+                x += WIDTH(c);
+            mfacts -= c->cfact;
 		} else {
-			h = (m->wh - y) / (n - i);
+			// h = (m->wh - y) / (n - i);
+			h = (m->wh - y) * (c->cfact / sfacts);
 			resize(c, x + m->wx, m->wy + y, m->ww - x - (2 * c->bw), h - (2 * c->bw), 0);
-			y += HEIGHT(c);
+			if (y + HEIGHT(c) < m->wh)
+                y += HEIGHT(c);
+            sfacts -= c->cfact;
 		}
 }
 
@@ -2384,4 +2398,3 @@ deck(Monitor *m)
 		else
 			resize(c, m->wx + mw, m->wy, m->ww - mw - (2*c->bw), m->wh - (2*c->bw), False);
 }
-
