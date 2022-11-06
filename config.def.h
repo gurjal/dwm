95f80a07ad3e3c718fd9c48f52db8a56edaf46b9
gurjal <shaanraya135@gmail.com>
2022-11-06 15:51:09 -0800

    apply movestack patch

--- config.def.h
+++ config.def.h
@@ -83,6 +83,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = -0.05} },
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
