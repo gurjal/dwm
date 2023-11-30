458f3823c3449e58f4fc81ca5f14b429149f3784
gurjal <gurjal@proton.me>
2023-11-30 11:04:01 -0800

    theme edits

--- themes/seoul256.h
+++ themes/seoul256.h
@@ -1,11 +1,19 @@
 /*
-* dwm/themes/seoul256.h
-*
-*/
+ * dwm/themes/seoul256.h
+ *
+ */
+
+// imo seoul256 looks better in lighter bg because it was designed to be a low
+// contrast theme
+//
+// values for a darker bg but now redundant because of gruvbox
+/* static const char normal_bg[] = "#3a3a3a"; */
+/* static const char normal_bd[] = "#3a3a3a"; */
+/* static const char select_bg[] = "#3a3a3a"; */
 
 static const char normal_fg[] = "#949494";
-static const char normal_bg[] = "#3a3a3a";
-static const char normal_bd[] = "#3a3a3a";
+static const char normal_bg[] = "#4B4B4B";
+static const char normal_bd[] = "#4B4B4B";
 static const char select_fg[] = "#d0d0d0";
-static const char select_bg[] = "#3a3a3a";
+static const char select_bg[] = "#4B4B4B";
 static const char select_bd[] = "#ffafaf";
